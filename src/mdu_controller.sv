eb4dda66d22c75c7b5d21af9b0928894858616fc
404allen404 <n26120935@gs.ncku.edu.tw>
2024-02-25 00:20:06 +0800

    Finish mul module

--- src/mdu_controller.sv
+++ src/mdu_controller.sv
@@ -4,22 +4,49 @@ module mdu_controller (
   input logic rst,
   input logic [2:0] funct3,
   input logic mdu_in_valid,
+  input logic cpu_busy,
   /* output */
-  output logic [1:0] md_alu_op;
+  output logic m_wen,
+  // output logic d_op,
+  // output logic d_sr,
+  // output logic d_sl,
+  // output logic d_wen,
+  output logic mdu_busy,
+  output logic mdu_out_valid
 );
 
-  enum bit [1:0] {INIT, WAIT_VALID, COMPUTE, DONE} state;
-  logic [4:0] cnt;
+  enum bit [1:0] {MUL_INIT, MUL_WAIT_VALID, MUL_COMPUTE, MUL_DONE} mul_state;
+  // enum bit [1:0] {DIV_INIT, DIV_WAIT_VALID} div_state;
+  // logic [4:0] div_cnt;
+  logic [5:0] mul_cnt;
 
-  // cnt
+  assign mdu_busy = (mul_state != MUL_WAIT_VALID);
+  assign m_wen = (mul_state == MUL_COMPUTE);
+  assign mdu_out_valid = (mul_state == MUL_DONE);
+
+  // mul_state
   always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
-      cnt <= 5'd0;
+      mul_state <= MUL_INIT;
+    end
+    else begin
+      case (mul_state)
+        MUL_INIT:       mul_state <= MUL_WAIT_VALID;
+        MUL_WAIT_VALID: mul_state <= (mdu_in_valid && ~funct3[2]) ? MUL_COMPUTE : MUL_WAIT_VALID;
+        MUL_COMPUTE:    mul_state <= (mul_cnt == 6'd63) ? MUL_DONE : MUL_COMPUTE;
+        MUL_DONE:       mul_state <= cpu_busy ? MUL_DONE : MUL_WAIT_VALID;
+      endcase
     end
   end
 
-
-
-
+  // mul_cnt
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      mul_cnt <= 6'd0;
+    end
+    else if (mul_state == MUL_COMPUTE) begin
+      mul_cnt <= mul + 6'd1;
+    end
+  end
 
 endmodule
--- src/mdu_top.sv
+++ src/mdu_top.sv
@@ -1,5 +1,6 @@
-`include "mdu_alu.sv"
-`include "mdu_"
+`include "m_alu.sv"
+`include "d_alu.sv"
+`include "mdu_controller.sv"
 
 module (
   /* input */
@@ -16,13 +17,88 @@ module (
   output logic mdu_busy
 );
 
+  logic funct3_r;
+  logic m_wen;
+  logic [63:0]  m_alu_out;
+  logic [63:0]  multiplicand;
+  logic [127:0] mul_res;
+  // logic [31:0]  divisor;
+  // logic [63:0]  div_res;
 
+  // mdu_out
+  always_comb begin
+    if (~funct3_r[2]) begin
+      mdu_out = (funct3_r[1:0] == 2'd0) ? mul_res[31:0] : mul_res[63:32];
+    end
+    else begin
+      mdu_out = 32'd0;
+    end
+  end
 
+  // funct3_r
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      funct3_r <= 3'd0;
+    end
+    else if (mdu_in_valid && ~mdu_busy) begin
+      funct3_r <= funct3;
+    end
+  end
 
+  // multiplicand
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      multiplicand <= 64'd0;
+    end
+    else if (mdu_in_valid && ~funct3[2] && ~mdu_busy) begin
+      if (funct3[1:0] == 2'b11) begin // unsigned
+        multiplicand <= {32'd0, mdu_in_1};
+      end
+      else begin // signed
+        multiplicand <= {{32{mdu_in_2[31]}}, mdu_in_2};
+      end
+    end
+  end
 
+  // mul_res
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      mul_res <= 128'd0;
+    end
+    else if (mdu_in_valid && ~funct3[2] && ~mdu_busy) begin
+      if (funct3[1]) begin // unsigned
+        mul_res <= {64'd0, mdu_in_2};
+      end
+      else begin // signed
+        mul_res <= {64'd0, {32{mdu_in_2[31]}}, mdu_in_2};
+      end
+    end
+    else if (m_wen) begin
+      mul_res <= {1'b0, m_alu_out, mul_res[63:1]};
+    end
+  end
 
+  // controller
+  mdu_controller mdu_controller_0 (
+    /* input */
+    .clk(clk),
+    .rst(rst),
+    .funct3(funct3),
+    .mdu_in_valid(mdu_in_valid),
+    .cpu_busy(cpu_busy),
+    /* output */
+    .m_wen(m_wen),
+    .mdu_busy(mdu_busy)
+  );
 
-
-
+  // m_alu
+  m_alu m_alu_0 (
+    /* input */
+    .m_alu_op(mul_res[0]),
+    .m_alu_in_1(mul_res[127:64]),
+    .m_alu_in_2(multiplicand),
+    /* output */
+    .m_alu_out(m_alu_out)
+  );
 
 endmodule
