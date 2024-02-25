198c8558d8153898d3b629e1cdc94626c3007cec
404allen404 <n26120935@gs.ncku.edu.tw>
2024-02-26 00:08:52 +0800

    Finish radix-4 multiplier

--- src/mdu_top.sv
+++ src/mdu_top.sv
@@ -4,8 +4,8 @@ module mdu_top(
   input logic rst,
   input logic mdu_in_valid,
   input logic [2:0] funct3,
-  input logic [31:0] mdu_in_1,
-  input logic [31:0] mdu_in_2,
+  input logic [31:0] mdu_in_1, // multiplicand
+  input logic [31:0] mdu_in_2, // multiplier
   input logic cpu_busy,
   /* output */
   output logic [31:0] mdu_out,
@@ -13,89 +13,30 @@ module mdu_top(
   output logic mdu_busy
 );
 
-  logic [2:0] funct3_r;
-  logic m_wen;
-  logic [63:0]  m_alu_out;
-  logic [63:0]  multiplicand;
-  logic [127:0] mul_res;
-  // logic [31:0]  divisor;
-  // logic [63:0]  div_res;
+  logic mul_in_valid;
+  logic [31:0] mul_out;
+  logic mul_out_valid;
+  logic mul_busy;
 
-  // mdu_out
-  always_comb begin
-    if (~funct3_r[2]) begin
-      mdu_out = (funct3_r[1:0] == 2'd0) ? mul_res[31:0] : mul_res[63:32];
-    end
-    else begin
-      mdu_out = 32'd0;
-    end
-  end
+  assign mul_in_valid = mdu_in_valid && ~funct3[2];
+  assign mdu_busy = mul_busy;
+  assign mdu_out_valid = mul_out_valid;
+  assign mdu_out = mul_out;
 
-  // funct3_r
-  always_ff @(posedge clk or posedge rst) begin
-    if (rst) begin
-      funct3_r <= 3'd0;
-    end
-    else if (mdu_in_valid && ~mdu_busy) begin
-      funct3_r <= funct3;
-    end
-  end
-
-  // multiplicand
-  always_ff @(posedge clk or posedge rst) begin
-    if (rst) begin
-      multiplicand <= 64'd0;
-    end
-    else if (mdu_in_valid && ~funct3[2] && ~mdu_busy) begin
-      if (funct3[1:0] == 2'b11) begin // unsigned
-        multiplicand <= {32'd0, mdu_in_1};
-      end
-      else begin // signed
-        multiplicand <= {{32{mdu_in_2[31]}}, mdu_in_1};
-      end
-    end
-  end
-
-  // mul_res
-  always_ff @(posedge clk or posedge rst) begin
-    if (rst) begin
-      mul_res <= 128'd0;
-    end
-    else if (mdu_in_valid && ~funct3[2] && ~mdu_busy) begin
-      if (funct3[1]) begin // unsigned
-        mul_res <= {96'd0, mdu_in_2};
-      end
-      else begin // signed
-        mul_res <= {64'd0, {32{mdu_in_2[31]}}, mdu_in_2};
-      end
-    end
-    else if (m_wen) begin
-      mul_res <= {1'b0, m_alu_out, mul_res[63:1]};
-    end
-  end
-
-  // controller
-  mdu_controller mdu_controller_0 (
+  // radix_4_mul
+  radix_4_mul radix_4_mul_0 (
     /* input */
     .clk(clk),
     .rst(rst),
-    .funct3(funct3),
-    .mdu_in_valid(mdu_in_valid),
+    .multiplier(mdu_in_2),
+    .multiplicand(mdu_in_1),
+    .mul_in_valid(mul_in_valid),
+    .mul_type(funct3[1:0]),
     .cpu_busy(cpu_busy),
     /* output */
-    .m_wen(m_wen),
-    .mdu_busy(mdu_busy),
-    .mdu_out_valid(mdu_out_valid)
-  );
-
-  // m_alu
-  m_alu m_alu_0 (
-    /* input */
-    .m_alu_op(mul_res[0]),
-    .m_alu_in_1(mul_res[127:64]),
-    .m_alu_in_2(multiplicand),
-    /* output */
-    .m_alu_out(m_alu_out)
+    .mul_out(mul_out),
+    .mul_out_valid(mul_out_valid),
+    .mul_busy(mul_busy)
   );
 
 endmodule
---
+++ src/radix_4_mul.sv
@@ -0,0 +1,111 @@
+module radix_4_mul (
+  /* input */
+  input logic clk,
+  input logic rst,
+  input logic mul_in_valid,
+  input logic [1:0] mul_type,
+  input logic [31:0] multiplier,
+  input logic [31:0] multiplicand,
+  input logic cpu_busy,
+  /* output */
+  output logic [31:0] mul_out,
+  output logic mul_out_valid,
+  output logic mul_busy
+);
+
+  enum bit [2:0] {MUL_WAIT_VALID, MUL_PRE_COMPUTE, MUL_COMPUTE, MUL_DONE} mul_state;
+
+  logic [34:0] neg_1_multiplicand_w;
+  logic [4:0]  cnt;
+  logic [34:0] op_vector;
+  logic [33:0] real_multiplier;
+  logic [33:0] real_multiplicand;
+  logic [67:0] neg_1_multiplicand;
+  logic [67:0] neg_2_multiplicand;
+  logic [67:0] pos_1_multiplicand;
+  logic [67:0] pos_2_multiplicand;
+  logic [67:0] product;
+
+  assign mul_busy = (mul_state != MUL_WAIT_VALID);
+  assign mul_out_valid = (mul_state == MUL_DONE);
+  assign mul_out = !mul_type ? product[31:0] : product[63:32];
+
+  // mul_state
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      mul_state <= MUL_WAIT_VALID;
+    end
+    else begin
+      case (mul_state)
+        MUL_WAIT_VALID:  mul_state <= mul_in_valid ? MUL_PRE_COMPUTE : MUL_WAIT_VALID;
+        MUL_PRE_COMPUTE: mul_state <= MUL_COMPUTE;
+        MUL_COMPUTE:     mul_state <= (cnt == 5'd16) ? MUL_DONE : MUL_COMPUTE;
+        MUL_DONE:        mul_state <= cpu_busy ? MUL_DONE : MUL_WAIT_VALID;
+      endcase
+    end
+  end
+
+  assign neg_1_multiplicand_w = ~{real_multiplicand[33], real_multiplicand} + 35'd1;
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      neg_1_multiplicand <= 68'd0;
+      neg_2_multiplicand <= 68'd0;
+      pos_1_multiplicand <= 68'd0;
+      pos_2_multiplicand <= 68'd0;
+      op_vector <= 35'd0;
+      product <= 68'd0;
+      cnt <= 5'd0;
+    end
+    else if (mul_state == MUL_PRE_COMPUTE) begin
+      neg_1_multiplicand <= {{33{neg_1_multiplicand_w[33]}}, neg_1_multiplicand_w};
+      neg_2_multiplicand <= {{33{neg_1_multiplicand_w[33]}}, neg_1_multiplicand_w} << 1;
+      pos_1_multiplicand <= {{34{real_multiplicand[32]}}, real_multiplicand};
+      pos_2_multiplicand <= {{34{real_multiplicand[32]}}, real_multiplicand} << 1;
+      op_vector <= {real_multiplier, 1'b0};
+      product <= 68'd0;
+      cnt <= 5'd0;
+    end
+    else if (mul_state == MUL_COMPUTE) begin
+      cnt <= cnt + 5'd1;
+      op_vector <= op_vector >> 2;
+      case (op_vector[2:0])
+        3'b010, 3'b001: product <= product + ((pos_1_multiplicand) << (cnt << 1));
+        3'b110, 3'b101: product <= product + ((neg_1_multiplicand) << (cnt << 1));
+        3'b111, 3'b000: product <= product;
+        3'b011:         product <= product + ((pos_2_multiplicand) << (cnt << 1));
+        3'b100:         product <= product + ((neg_2_multiplicand) << (cnt << 1));
+      endcase
+    end
+  end
+
+  // real_multiplier
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      real_multiplier <= 34'd0;
+    end
+    else if(mul_state == MUL_WAIT_VALID && mul_in_valid) begin
+      if (mul_type[1]) begin
+        real_multiplier <= {2'd0, multiplier};
+      end
+      else begin
+        real_multiplier <= {{2{multiplier[31]}}, multiplier};
+      end
+    end
+  end
+
+  // real_multiplicand
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      real_multiplicand <= 34'd0;
+    end
+    else if (mul_state == MUL_WAIT_VALID && mul_in_valid) begin
+      if (mul_type == 2'b11) begin
+        real_multiplicand <= {2'd0, multiplicand};
+      end
+      else begin
+        real_multiplicand <= {{2{multiplicand[31]}}, multiplicand};
+      end
+    end
+  end
+
+endmodule
